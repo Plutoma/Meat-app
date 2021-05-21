b95441888d3ffd051ea2e8a56536a075a108fb28
Plutoma <92670968+Plutoma@users.noreply.github.com>
2021-05-21 20:00:21 +0000

    ACPI / PM: Fix resource_lock dead lock in acpi_power_on_device

--- lib/ruby-band/apache/stat/correlation.rb
+++ lib/ruby-band/apache/stat/correlation.rb
@@ -6,4 +6,28 @@ module Apache
 
       java_import "org.apache.commons.math3.stat.correlation.Covariance"
       java_import "org.apache.commons.math3.stat.correlation.PearsonsCorrelation"
-      java_import "org.apache.commons.math3.stat.correlation.S
+      java_import "org.apache.commons.math3.stat.correlation.SpearmansCorrelation"
+
+      # Calculate covariance between two Numeric arrays
+      # * *Args*    :
+      #   - +Array1+ -> must be a RubyArray.
+      #   - +Array2+ -> must be a RubyArray.
+      def self.covariance(array_1,array_2)
+        obj = Covariance.new
+        result = obj.covariance(array_1.to_java(:double),array_2.to_java(:double))
+        result
+      end
+
+      # Calculate Pearson correlation between two Numeric arrays
+      # * *Args*    :
+      #   - +Array1+ -> must be a RubyArray.
+      #   - +Array2+ -> must be a RubyArray.
+      def self.pearson_correlation(array_1,array_2)
+        obj = PearsonsCorrelation.new
+        result = obj.correlation(array_1.to_java(:double),array_2.to_java(:double))
+        result
+      end
+
+      # Calculate Spearman correlation between two Numeric arrays
+      # * *Args*    :
+      #   - +Array1+ -> m
