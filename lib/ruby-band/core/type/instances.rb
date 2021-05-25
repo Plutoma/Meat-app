c119176b2577a100c1c8048b801f12b5c5961488
Plutoma <92670968+Plutoma@users.noreply.github.com>
2021-05-25 22:29:00 +0000

    Update exportvariant_skills2.html

--- lib/ruby-band/core/type/instances.rb
+++ lib/ruby-band/core/type/instances.rb
@@ -10,4 +10,24 @@ module Core
     java_import "weka.core.Instances"
     java_import 'java.io.File'
     java_import 'weka.core.converters.CSVSaver'
-    java_import 'weka.core.converters.
+    java_import 'weka.core.converters.ArffSaver'
+    java_import "weka.core.FastVector"
+    java_import "weka.core.DenseInstance"
+
+    #
+    # * *Description*    :
+    # This is the main class from the Weka package for data handling. It is essentially a matrix: each row
+    # is an instance of the 'Instance' class, while each column is an instance of the 'Attribute' class
+    # The class 'Instances' is here extended to add custom functionalities 
+    class Instances
+
+      # Convert an Instances object to a bidimensional Ruby array
+      # where each row corresponds to an Instance object
+      def to_a2d
+        matrix = Array.new
+        att = Array.new
+        self.enumerateAttributes.each_with_index do |a,idx|
+          if a.isNumeric  
+            enumerate_instances.each {|s| att << s.value(s.attribute(idx))}
+            matrix << att
+            att = A
