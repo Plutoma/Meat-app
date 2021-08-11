85452afc61225bbe9c3f5240280b36a921622dd3
Plutoma <92670968+Plutoma@users.noreply.github.com>
2021-08-11 09:57:39 +0000

    Update base_site.html

--- lib/ruby-band/weka/classifiers/lazy/lazy.rb
+++ lib/ruby-band/weka/classifiers/lazy/lazy.rb
@@ -1,4 +1,16 @@
 require 'ruby-band/weka/classifiers/class_builder'
 
 # The Weka::Classifiers::Lazy module contains classifiers from the 'weka.classifiers.lazy' package
-# New weka class
+# New weka classes can be defined by adding them to the build_classes method.
+#
+module Weka
+  module Classifiers
+    module Lazy
+      include ClassBuilder
+
+      build_classes :IBk,
+                    :KStar,
+                    :LWL
+    end
+  end
+end
---
+++ lib/ruby-band/weka/classifiers/meta/meta.rb
@@ -0,0 +1,15 @@
+require 'ruby-band/weka/classifiers/class_builder'
+
+# The Weka::Classifiers::Meta module contains classifiers from the 'weka.classifiers.meta' package
+# New weka classes can be defined by adding them to the build_classes method.
+module Weka
+  module Classifiers
+    module Meta
+      include ClassBuilder
+
+      build_classes :AdaBoostM1,
+                    :AdditiveRegression,
+                    :AttributeSelectedClassifier,
+                    :Bagging,
+                    :ClassificationViaRegression,
+                    :CostSe
