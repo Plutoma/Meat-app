bb8a12612e2a48ba3b042561d64904b66d6a8059
Plutoma <92670968+Plutoma@users.noreply.github.com>
2021-08-13 23:02:16 +0000

    added Dockerfile

--- lib/ruby-band/weka/clusterers/clusterers.rb
+++ lib/ruby-band/weka/clusterers/clusterers.rb
@@ -22,3 +22,44 @@ module Weka
           end
         end
       end
+    end
+
+    class EM
+      include Utils
+      class Base < EM
+        def initialize(&block)
+          super
+          if block_given?
+            init_instance_clusterer(&block)
+          else
+            init_clusterer
+          end
+        end
+      end
+    end
+
+    class HierarchicalClusterer
+      include Utils
+      class Base < HierarchicalClusterer
+        def initialize(&block)
+          super
+          if block_given?
+            init_instance_clusterer(&block)
+          else
+            init_clusterer
+          end
+        end
+      end
+    end
+
+    class SimpleKMeans
+      include Utils
+      class Base < SimpleKMeans
+        def initialize(&block)
+          super
+          if block_given?
+            init_instance_clusterer(&block)
+          else
+            init_clusterer
+          end
+       
