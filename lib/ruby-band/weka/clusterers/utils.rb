d7a3a0cbb12293ec06698e67ff36501194d7fbca
Plutoma <92670968+Plutoma@users.noreply.github.com>
2021-08-18 09:14:53 +0000

    Pontoon: Update Slovenian (sl) localization of Fundraising

--- lib/ruby-band/weka/clusterers/utils.rb
+++ lib/ruby-band/weka/clusterers/utils.rb
@@ -14,4 +14,38 @@ module Weka
 
       def init_instance_clusterer(&block)
         self.instance_eval(&block)
-        #@dataset.setClassIndex(@class
+        #@dataset.setClassIndex(@class_index)
+        buildClusterer(@dataset)
+      end
+
+      #Instance methods list
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      #set instance data for the clusterer
+      def set_data(data)
+        @dataset = data
+      end
+
+      #set options for the clusterer
+      def set_options(options)
+        options_inst = Utils.splitOptions(options)
+        setOptions(options_inst)
+      end
+
+      def list_options
+        listOptions.map {|key| "#{key.synopsis} #{key.description}"}.join("\n")
+      end
+
+      #the description provided by the Weka Documentation
+      def description
+        globalInfo
+      end
+
+      #list cluster centroids with coordinates
+      def get_centroids
+        getClusterCentroids
+      end
+
+      #list cluster's capabilities with attribu
