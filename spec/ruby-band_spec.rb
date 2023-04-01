05a5ec96778a693896d9eb551b2504b5d4de9fd4
Plutoma <92670968+Plutoma@users.noreply.github.com>
2023-04-01 09:25:15 +0000

    Updated corp-history.md

--- spec/ruby-band_spec.rb
+++ spec/ruby-band_spec.rb
@@ -2,4 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "BioBand" do
   it "fails" do
-    fail "hey buddy, you should probably rename this
+    fail "hey buddy, you should probably rename this file and start specing for real"
+  end
+end
